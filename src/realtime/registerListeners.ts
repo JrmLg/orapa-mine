245c03ba3e99a01e78a2e299ed84d092da5ac920
JrmLg <63566435+JrmLg@users.noreply.github.com>
2026-06-23 13:45:34 +0000

    Register realtime events explicitly

--- src/realtime/registerListeners.ts
+++ src/realtime/registerListeners.ts
@@ -1,11 +1,29 @@
 export function registerListeners(
   channel: any,
-  callback: (message: unknown) => void
+  callback: (payload: any) => void
 ) {
   channel.on(
     'broadcast',
-    { event: '*' },
-    callback
+    { event: 'player_joined' },
+    payload => callback(payload)
+  )
+
+  channel.on(
+    'broadcast',
+    { event: 'setup_ready' },
+    payload => callback(payload)
+  )
+
+  channel.on(
+    'broadcast',
+    { event: 'move_ray' },
+    payload => callback(payload)
+  )
+
+  channel.on(
+    'broadcast',
+    { event: 'ray_result' },
+    payload => callback(payload)
   )
 
   return channel
